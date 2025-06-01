587c5c8a0ead0171cbb0b4353bdf091e04359794
chandruquantzi2 <chandru.g@quantzi.in>
2025-06-01 12:58:17 +0530

    nex next js

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="text-center">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple homepage.</p>
+    </div>
+  );
+}
+
+export default Home;
