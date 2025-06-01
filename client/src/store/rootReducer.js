56203b2115a4b4629edda0f84a09f3b38550da42
chandruquantzi2 <chandru.g@quantzi.in>
2025-06-01 12:09:32 +0530

    nex next js

--- client/src/store/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import admin from './admin/reducer';
-import company from './company/reducer';
-
-export default combineReducers({
-    admin,
-    company
-});
